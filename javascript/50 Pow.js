bf2657f74003ce37728720f66342f1ef2ec8ee50
Ifo Ikede <ifoitech@gmail.com>
2022-08-30 02:35:00 -0300

    leetcode pow solution

---
+++ javascript/50 Pow.js
@@ -0,0 +1,18 @@
+/**
+ *  https://leetcode.com/problems/powx-n/
+ * 
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+ var myPow = function(x, n) {
+    //base case
+ if (n === 0)   { 
+   return 1 ;
+   
+ 		} else {
+       return ( x * myPow(x, n-1) ) ;  //  if(n > 0) return  x * myPow(x, n-1) ;
+     
+   } 
+};
+};
